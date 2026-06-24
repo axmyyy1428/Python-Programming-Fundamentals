afee872900b60576e694eaf5d41cd486881050e9
axmyyy1428 <ayeshamuskan1428@gmail.com>
2026-06-24 17:29:52 +0500

    Add result summarizer function for student results

---
+++ University_Result_Management/result_summarizer.py
@@ -0,0 +1,7 @@
+def result_summarizer(marks,call_1,call_2,call_3,name,roll_number):
+    print('Name: ',name)
+    print('Roll Number: ',roll_number)
+    print(call_1)
+    print('Total Marks:',sum(marks))
+    print('GPA: ',call_2)
+    print(f'You are {call_3}!')
